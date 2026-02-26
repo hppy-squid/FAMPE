4fbc254f8cb5a9282f5bf2fd732c24579d4e1f81
Alice Fearn <alice.fearn@hotmail.com>
2026-02-26 13:07:05 +0100

    projekter + map

---
+++ Exam/app/src/main/java/com/FAMPE/fampe/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.FAMPE.fampe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.FAMPE.fampe.ui.theme.FampeTheme
+import com.google.maps.android.compose.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GoogleMap(
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FampeTheme {
+        Greeting("Android")
+    }
+}
