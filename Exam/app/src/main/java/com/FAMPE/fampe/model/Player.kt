e201d0ced1b3dd13fcd06158e55d832e020f5d0a
Alice Fearn <alice.fearn@hotmail.com>
2026-03-13 12:21:35 +0100

    dra ner position från databas

--- Exam/app/src/main/java/com/FAMPE/fampe/model/Player.kt
+++ Exam/app/src/main/java/com/FAMPE/fampe/model/Player.kt
@@ -1,8 +1,11 @@
 package com.FAMPE.fampe.model
 
+import com.google.firebase.firestore.GeoPoint
+
 data class Player(
     val id: String = "",
-    val lat: Double = 0.0,
-    val lng: Double = 0.0,
-    val updatedAt: Long = 0
+    val location: GeoPoint = GeoPoint(0.0, 0.0),
+    val lastUpdated: Long = 0,
+    val name: String = "",
+    val score: Int = 0
 )
--- Exam/app/src/main/java/com/FAMPE/fampe/viewmodel/MapViewModel.kt
+++ Exam/app/src/main/java/com/FAMPE/fampe/viewmodel/MapViewModel.kt
@@ -1,12 +1,41 @@
 package com.FAMPE.fampe.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.FAMPE.fampe.model.Player
 import com.FAMPE.fampe.repository.PlayerRepository
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MapViewModel  : ViewModel() {
     private val repository = PlayerRepository()
 
     fun updateLocation(userId: String, lat: Double, lng: Double) {
         repository.updateLocation(userId, lat, lng)
+    }
+
+    fun listenToPlayers(onUpdate: (List<Player>) -> Unit) {
+
+        FirebaseFirestore.getInstance()
+            .collection("players")
+            .addSnapshotListener { snapshot, _ ->
+
+                if (snapshot == null) return@addSnapshotListener
+
+                val players = snapshot.documents.mapNotNull { doc ->
+
+                    val location = doc.getGeoPoint("location") ?: return@mapNotNull null
+                    val name = doc.getString("name") ?: ""
+                    val score = doc.getLong("score")?.toInt() ?: 0
+
+                    Player(
+                        id = doc.id,
+                        location = location,
+                        name = name,
+                        score = score
+                    )
+                }
+
+                onUpdate(players)
+            }
+
     }
 }
